44ca037bb93b1d25f09feafb8f82d714fc7a071b
JuliusBright <juliasbright@gmail.com>
2023-10-02 10:36:17 +0200

    Develop (#2)
    * Final Work
    
    * Readme
    
    * Patch Fix
    
    * Postman Added

---
+++ Employee-Backend/Db/EmployeeDbContext.cs
@@ -0,0 +1,11 @@
+
+using Microsoft.EntityFrameworkCore;
+
+public class EmployeeDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+
+    public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+    {
+    }
+}
